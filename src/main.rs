8552fbd4f0ae2be8fbcf193ed869acb824d27920
azdharsyahputra <if23.muhammadsyahputra@mhs.ubpkarawang.ac.id>
2025-10-05 21:55:34 +0700

    belajar rust day 3

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,56 @@
-fn main() {
-    let s1 = String::from("hello");
+const AJAR: i32 = 10;
 
-    let (s2, len) = calculate_length(s1);
-
-    println!("The length of '{s2}' is {len}.");
+fn main(){
+    println!("{}",AJAR);
+    variable_scope();
+    stack_heap();
+    string();
+    string_type();
 }
 
-fn calculate_length(s: String) -> (String, usize) {
-    let length = s.len();
+fn variable_scope(){
+    let ajar = 1;
 
-    (s, length)
+    {
+        let raja = 2;
+
+        println!("inner scope {}",raja);
+    }
+
+    println!("inner scope {}",ajar);
+}
+
+fn stack_heap(){
+    function_a();
+    function_b();
+}
+
+fn function_a(){
+    let a = 10;
+    let b = String::from("ajar");
+
+    println!("{}{}", a,b);
+}
+fn function_b(){
+    let a = 10;
+    let b = String::from("aja");
+    println!("{}{}", a,b);
+}
+fn string(){
+    let name: &str = " Ajar ";
+    let trim: &str = name.trim();
+
+    println!("{}",trim)
+}
+
+fn string_type(){
+    let mut name: String = String::from("Ajar");
+
+    println!("{}", name);
+
+    name.push_str(" Syahputra");
+    println!("{}",name);
+
+    let diana = name.replace("Ajar", "Diana");
+    println!("{}",diana);
 }
